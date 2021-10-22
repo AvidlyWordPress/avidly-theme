795739e87cc8777bf38f70923e3a5454f13f3585
Veera Peltomäki <veera.peltomaki@avidlyagency.com>
2021-10-22 16:59:22 +0300

    Fix post type detection from card

--- template-parts/card/content.php
+++ template-parts/card/content.php
@@ -8,9 +8,9 @@
  * @since 1.0.0
  */
 
-$article_class     = 'card mx-2 border h-full link-element';
-$article_class    .= ( is_sticky() && ! is_paged() ) ? ' sm:flex' : ' block sm:flex md:block';
-$posttype = get_post_type_object( get_post_type() );
+$article_class  = 'card mx-2 border h-full link-element';
+$article_class .= ( is_sticky() && ! is_paged() ) ? ' sm:flex' : ' block sm:flex md:block';
+$posttype       = get_post_type_object( get_post_type() );
 ?>
 
 <article <?php post_class( $article_class ); ?> aria-labelledby="post-<?php the_ID(); ?>">
@@ -31,11 +31,11 @@ $posttype = get_post_type_object( get_post_type() );
 
 	<div class="card__wrapper p-4 flex-1">
 		<header class="card__header mb-4">
-			<div class="flex flex-col sm:flex-row flex-wrap alignwide sm:items-center">
+			<div class="flex flex-wrap alignwide items-center">
 				<?php
 				// Display post type name in search results.
 				if ( is_search() && $posttype ) {
-					echo '<span class="card__posttype text-sm">' . esc_html( $post_type->labels->singular_name ) . '</span>';
+					echo '<span class="card__posttype text-sm">' . esc_html( $posttype->labels->singular_name ) . '</span>';
 				}
 
 				if ( has_category() ) {
@@ -44,7 +44,7 @@ $posttype = get_post_type_object( get_post_type() );
 
 					echo sprintf(
 						'%s<span class="card__categories text-sm">%s</span>',
-						( is_search() && $posttype ) ? '<span class="hidden sm:block mx-2">|</span>' : '', // phpcs:ignore.
+						( is_search() && $posttype ) ? '<span class="mx-2">|</span>' : '', // phpcs:ignore.
 						sprintf(
 							/* translators: %s post category. */
 							_x( '<span class="screen-reader-text">In category </span>%s', 'theme UI', 'avidly-theme' ), // phpcs:ignore.
