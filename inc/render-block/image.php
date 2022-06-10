bb8719b818196dfddfe5b3d24c7de92143ee47cf
Veera Peltomäki <veera.peltomaki@avidlyagency.com>
2022-06-10 16:00:42 +0300

    Improve image block structure change + add style support

--- inc/render-block/image.php
+++ inc/render-block/image.php
@@ -1,44 +1,44 @@
 <?php
 /**
- * Change default render: Image block
+ * Block render callback: Image
  *
  * @package Avidly_Theme
  * @since 2.0.0
  */
 
-add_filter( 'render_block', 'avidly_theme_image_block', 10, 2 );
+add_filter( 'render_block_core/image', 'avidly_theme_image_block', 10, 2 );
 
 /**
- * Create new output for image block.
- * NOTE: This only changes block sturcture in front end.
+ * Modify block output.
+ * NOTE: This only changes block strurture in frontend.
  * Support for WP 6.0 where wrapper div was removed from image block.
  *
  * @param string $block_content The block content about to be appended.
- * @param array $block The full block, including name and attributes.
+ * @param array  $block The full block, including name and attributes.
  *
  * @link https://developer.wordpress.org/reference/hooks/render_block/
  */
 function avidly_theme_image_block( $block_content, $block ) {
 
-	// Get block name.
-	$block_name = ( isset( $block['blockName'] ) ) ? $block['blockName'] : 'unknown';
-
-	// Return if not gallery orimage block.
-	if ( 'core/image' !== $block_name ) {
-		return $block_content;
-	}
-
-	// Fallback, return original output for image blocks added before WP 6.0 update.
+	// Fallback for WP < 6.0.
+	// Return original output if image block is added before WP 6.0 update.
 	if ( substr( $block_content, 1, 7 ) !== '<figure' ) { // Detect if block content starts with <figure.
 		return $block_content;
 	}
 
-	// Remove .wp-block-image class from figure, we will reuse it in custom wrapper.
-	$block_content = str_replace('<figure class="wp-block-image', '<figure class="wp-block-image__content', $block_content );
+	// Wrap figure content inside a span.
+	$block_content = str_replace( '<img', '<span><img', $block_content );
+	$block_content = str_replace( '</figure>', '</span></figure>', $block_content );
+
+	// Detect aligns.
+	$align = isset( $block['attrs']['align'] ) ? $block['attrs']['align'] : '';
+
+	// Move left&right align class from <figure> into <span>.
+	if ( 'left' === $align || 'right' === $align ) {
+		$block_content = str_replace( ' align' . $align, '', $block_content );
+		$block_content = str_replace( '<span>', '<span class="align' . esc_attr( $align ) . '">', $block_content );
+	}
 
 	// Return modified output.
-	return sprintf(
-		'<div class="wp-block-image">%s</div>',
-		$block_content
-	);
+	return $block_content;
 }
